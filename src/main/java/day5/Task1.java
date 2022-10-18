23e3d15a7b806ba7b4bdb8e0dfdf1c8d51107b20
Ekaterina Prudnikova <rvsn0913@mail.ru>
2022-10-18 16:40:28 +0300

    День 5

--- src/main/java/day5/Task1.java
+++ src/main/java/day5/Task1.java
@@ -1,7 +1,19 @@
 package day5;
 
 public class Task1 {
+//    1. Создать класс Автомобиль (англ. Car) с полями “Модель”, “Цвет”, “Год выпуска”.
+//    Создать get и set методы для каждого поля. Создать экземпляр класса Автомобиль. Задать значение для каждого поля, используя set методы.
+//    Вывести в консоль значение каждого из полей, используя get методы.
+//Созданный вами класс должен отвечать принципам инкапсуляции.
+
     public static void main(String[] args) {
+        Car car=new Car();
+        car.setModel("BMW");
+        car.setColor("Green");
+        car.setYear(2020);
+        System.out.println(car.getModel());
+        System.out.println(car.getColor());
+        System.out.println(car.getYear());
 
     }
 }

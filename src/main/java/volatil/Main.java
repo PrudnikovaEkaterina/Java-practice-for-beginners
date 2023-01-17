47419cb48595d960f8e241b0e87a349a61719223
Ekaterina Prudnikova <rvsn0913@mail.ru>
2023-01-17 16:59:00 +0300

    fix

--- src/main/java/volatil/Main.java
+++ src/main/java/volatil/Main.java
@@ -4,6 +4,10 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+//        Ключевое слово volatile нужно обязательно использовать, когда 1 поток записывает в переменную,
+//        а 2-й  - читает из переменной, чтобы обеспечить когерентность (целостность) кэша и чтобы
+//        у разных потоков была одна и таже вресия переменной, которую они будут использовать.
+//        -
         MyThread myThread=new MyThread();
         myThread.start();
         Scanner scanner=new Scanner(System.in);
